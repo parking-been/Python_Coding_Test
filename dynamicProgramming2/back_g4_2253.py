32ab0990fdba5d80cf63042d32f91022dcacc0ce
parking-been <ysbeen00@gmail.com>
2026-01-05 12:54:47 +0900

    feat : backjoon study 01.05-2

--- dynamicProgramming2/back_g4_2253.py
+++ dynamicProgramming2/back_g4_2253.py
@@ -2,7 +2,7 @@ import sys
 
 N, M = map(int ,sys.stdin.readline().split())
 
-dp = [[1e9]*(N+1) for _ in range(N+1)]
+dp = [[1e9]*(int((N*2)**0.5)+2) for _ in range(N+1)]
 
 small = set()
 for i in range(M):
@@ -13,7 +13,7 @@ dp[1][0] = 0
 for i in range(2, N+1):
     if i in small:
         continue
-    for j in range(1, N):
+    for j in range(1, int((N*2)**0.5)+1):
         dp[i][j] = min(dp[i-j][j-1],dp[i-j][j],dp[i-j][j+1])+1
 
 if min(dp[N]) == 1e9:
